c182a60f390c4c76fca591bd00efe5edb9b7b150
roseliux <roxdurazo@gmail.com>
2017-07-23 23:30:35 -0700

    skip mounting the omniauth_callbacks.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get 'home/not_auth'
 
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
