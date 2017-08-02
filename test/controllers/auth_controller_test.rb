00195b711468b8c69c51164b1a0361b751d3eca0
roseliux <roxdurazo@gmail.com>
2017-08-01 23:29:34 -0700

    Accepted params can be customized using the devise_parameter_sanitizer system, add name. Create test for registration.

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -2,16 +2,29 @@ require 'test_helper'
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @valid_header = {
-      email: 'rox@mail.com',
-      password: 'secret123'
-    }
+    @email = users(:rox).email
+    @name = 'Jon'
+    @new_email = 'jondoe@mail.com'
+    @password = 'secret123'
+    @valid_header = { email: @email, password: @password }
   end
 
   test "should signin" do
     post new_user_session_url, headers: @valid_header, as: :json
     assert_equal 200, response.status
     assert_equal 'rox@mail.com', response.header['uid']
+  end
+
+  test "should Email registration" do
+    post user_registration_url,
+         params: { email: @new_email,
+                   password: @password,
+                   password_confirmation: @password,
+                   name: @name }, as: :json
+    payload = response_body
+    assert_equal 200, response.status
+    assert_equal @new_email, payload['data']['email']
+    assert_equal @name, payload['data']['name']
   end
 
 end
