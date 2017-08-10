62931993f6c798a0313d9983098a4e6621bacf97
roseliux <roxdurazo@gmail.com>
2017-08-09 22:30:18 -0700

    Ignore secrets file. Add omniauth routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root 'home#guest'
 
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
