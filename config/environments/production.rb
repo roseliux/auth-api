c466749744241d24a3e6ee9b98af2f6f247c7498
roseliux <roxdurazo@gmail.com>
2017-08-13 18:39:51 -0700

    Set mail on production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,10 +54,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -80,4 +76,24 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'new-auth-api.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => 'utf-8'
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'new-auth-api.herokuapp.com',
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: ENV['gmail_mail'],
+      password: ENV['gmail_password']
+  }
+
+  # OmniAuth.config.full_host = 'http://new-auth-api.herokuapp.com'
 end
